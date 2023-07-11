b7d8d761de82e1f882c7819dc4d1c2f11751cab7
SHOMA <c0b22070e1@edu.teu.ac.jp>
2023-07-11 16:14:00 +0900

    共通基本機能

--- heborisu.py
+++ heborisu.py
@@ -1,100 +1,103 @@
-import tkinter as tk
+import pygame
 import random
 
-SIZE = 30       #ブロックのサイズ
-moveX = 4       #テトロミノ表示位置（横）
-moveY = 0       #テトロミノ表示位置（縦）
-type = random.randint(0, 6)        #テトロミノのタイプ
+# ゲーム画面の設定
+WIDTH, HEIGHT = 400, 650
+GRID_SIZE = 30
+GRID_WIDTH, GRID_HEIGHT = WIDTH // GRID_SIZE, HEIGHT // GRID_SIZE
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+clock = pygame.time.Clock()
 
-timer = 800     #ゲームスピードコントロール
+# テトリスのブロックの定義
+tetriminos = [
+    [[1, 1, 1, 1]],
+    [[1, 1], [1, 1]],
+    [[1, 0, 0], [1, 1, 1]],
+    [[0, 0, 1], [1, 1, 1]],
+    [[0, 1, 1], [1, 1, 0]],
+    [[1, 1, 0], [0, 1, 1]],
+    [[0, 1, 0], [1, 1, 1]]
+]
 
-color = ["magenta", "blue", "cyan", "yellow", "orange", "red", "green", "black", "white"]
+# テトリスのブロックを表すクラス
+class Tetrimino:
+    def __init__(self):
+        self.shape = random.choice(tetriminos)
+        self.color = (random.randint(50, 255), random.randint(50, 255), random.randint(50, 255))
+        self.x = GRID_WIDTH // 2 - len(self.shape[0]) // 2
+        self.y = 0
 
-#テトロミノデータ
-tetroT = [-1, 0, 0, 0, 1, 0, 0, 1]
-tetroJ = [-1, 0, 0, 0, 1, 0, 1, 1]
-tetroI = [-1, 0, 0, 0, 1, 0, 2, 0]
-tetroO = [ 0, 0, 1, 0, 0, 1, 1, 1]
-tetroL = [-1, 0, 0, 0, 1, 0,-1, 1]
-tetroZ = [-1,-1, 0,-1, 0, 0, 1, 0]
-tetroS = [ 0, 0, 1, 0, 0, 1,-1, 1]
-tetro = [tetroT, tetroJ, tetroI, tetroO, tetroL, tetroZ, tetroS]
+    def rotate(self):
+        self.shape = list(zip(*reversed(self.shape)))
 
-#フィールドデータ
-field = []
-for y in range(22):
-    sub = []
-    for x in range(12):
-        if x==0 or x==11 or y==21 :
-            sub.append(8)
-        else :
-            sub.append(7)
-    field.append(sub)
+    def move(self, dx, dy):
+        self.x += dx
+        self.y += dy
 
-#テトロミノを表示する関数
-def drawTetris():
-    for i in range(4):
-        x = (tetro[type][i*2]+moveX)*SIZE
-        y = (tetro[type][i*2+1]+moveY)*SIZE
-        can. create_rectangle(x, y, x+SIZE, y+SIZE, fill=color[type])
+    def draw(self):
+        for y, row in enumerate(self.shape):
+            for x, col in enumerate(row):
+                if col:
+                    pygame.draw.rect(screen, self.color, (self.x * GRID_SIZE + x * GRID_SIZE,
+                                                          self.y * GRID_SIZE + y * GRID_SIZE,
+                                                          GRID_SIZE, GRID_SIZE))
 
-#フィールドを表示する関数
-def drawField():
-    for i in range(21):
-        for j in range(12):
-            outLine=0 if color[field[i+1][j]]=="white" else 1   #白いブロックは枠無しで表示
-            can.create_rectangle(j*SIZE, i*SIZE, (j+1)*SIZE, (i+1)*SIZE, fill=color[field[i+1][j]], width=outLine)
+    def collides(self, grid):
+        for y, row in enumerate(self.shape):
+            for x, col in enumerate(row):
+                if col and (self.y + y >= GRID_HEIGHT or self.x + x < 0 or self.x + x >= GRID_WIDTH or
+                            grid[self.y + y][self.x + x]):
+                    return True
+        return False
 
+    def place(self, grid):
+        for y, row in enumerate(self.shape):
+            for x, col in enumerate(row):
+                if col:
+                    grid[self.y + y][self.x + x] = self.color
 
+# ゲームの初期化
+def init_game():
+    grid = [[None] * GRID_WIDTH for i in range(GRID_HEIGHT)]
+    tetrimino = Tetrimino()
+    game_over = False
+    return grid, tetrimino, game_over
 
-def judge(afterX, afterY, afterTetro):  #アタリ判定をする関数
-    global moveX, moveY
-    result = True
-    for i in range(4):
-        x = afterTetro[i*2]+afterX
-        y = afterTetro[i*2+1]+afterY
-        if field[y+1][x]!=7 :
-            result = False
-    if result==True :
-        moveX = afterX
-        moveY = afterY
-        tetro[type].clear()
-        tetro[type].extend(afterTetro)
-    return result
+# ゲームのメインループ
+def run_game():
+    grid, tetrimino, game_over = init_game()
 
-def dropTetris():
-    global moveX, moveY, type, timer
-    afterTetro = []
-    afterTetro.extend(tetro[type])
-    result = judge(moveX, moveY+1, afterTetro)
-    if result==False :
-        for i in range(4):
-            x = tetro[type][i*2]+moveX
-            y = tetro[type][i*2+1]+moveY
-            field[y+1][x] = type
-        type = random.randint(0, 6)
-        moveX = 4
-        moveY = 0
-    can.after(timer, dropTetris)
-    timer -= 2                          #落下速度コントロール
-    if timer<140 :
-        timer = 180
+    while not game_over:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                return
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    tetrimino.move(-1, 0)
+                    if tetrimino.collides(grid):
+                        tetrimino.move(1, 0)
+                elif event.key == pygame.K_RIGHT:
+                    tetrimino.move(1, 0)
+                    if tetrimino.collides(grid):
+                        tetrimino.move(-1, 0)
+        tetrimino.move(0, 1)
+        if tetrimino.collides(grid):
+            tetrimino.move(0, -1)
+            tetrimino.place(grid)
+            tetrimino = Tetrimino()
+            if tetrimino.collides(grid):
+                game_over = True
 
-win = tk.Tk()
-win.geometry("340x630")
-can = tk.Canvas(win, width=12*SIZE, height=21*SIZE)
-can.place(x=-10, y=0)
-var = tk.StringVar()
-lab = tk.Label(win, textvariable=var, fg="blue", bg="white", font=("", "20"))   #得点表示
-lab.place(x=50, y=600)
+        screen.fill((0, 0, 0))
+        for y, row in enumerate(grid):
+            for x, col in enumerate(row):
+                if col:
+                    pygame.draw.rect(screen, col, (x * GRID_SIZE, y * GRID_SIZE, GRID_SIZE, GRID_SIZE))
 
+        tetrimino.draw()
+        pygame.display.flip()
+        clock.tick(3)
 
-def gameLoop():
-    can.delete("all")
-    drawField()
-    drawTetris()
-    can.after(50, gameLoop)
-
-gameLoop()
-dropTetris()
-win.mainloop()
+# ゲームの実行
+run_game()
